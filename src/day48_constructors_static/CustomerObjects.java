cd936f72fdd9163927d188f13f2bf7d4eba6fb65
FaithSlade <faithwslade@gmail.com>
2021-05-29 08:40:15 -0700

    Added update CustomerObjects class

--- src/day48_constructors_static/CustomerObjects.java
+++ src/day48_constructors_static/CustomerObjects.java
@@ -8,5 +8,12 @@ public class CustomerObjects {
         cs1.setId(1);
         cs1.setName("Bob");
         System.out.println(cs1);
+
+        //with Constructor: help you initialize and can do just one line
+        Customer cs2 = new Customer("Mike", 2); //created object and assign values in the same statement
+        Customer cs3 = new Customer("Faith", 4873);
+        System.out.println(cs2);
+        System.out.println(cs3);
+
     }
 }

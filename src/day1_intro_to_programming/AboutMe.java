e9439254e3cf6912478a0cfd71bc068d400bd687
FaithSlade <faithwslade@gmail.com>
2021-05-29 07:33:51 -0700

    Added package day1 all done

--- src/day1_intro_to_programming/AboutMe.java
+++ src/day1_intro_to_programming/AboutMe.java
@@ -1,4 +1,7 @@
 package day1_intro_to_programming;
 
 public class AboutMe {
+    public static void main(String[] arge) {
+        System.out.println("Hello my name is Faith, I live in San Francisco East Bay CA");
+    }
 }

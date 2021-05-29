5eb15a2d3288f701a40c023b4538eef77b864ef4
FaithSlade <faithwslade@gmail.com>
2021-05-29 08:29:51 -0700

    Added new CustomerObjects class

--- src/day48_constructors_static/CustomerObjects.java
+++ src/day48_constructors_static/CustomerObjects.java
@@ -2,6 +2,11 @@ package day48_constructors_static;
 
 public class CustomerObjects {
     public static void main(String[] args) {
-
+        Customer cs1 = new Customer();
+        System.out.println(cs1.toString());//print with default values that are set in no-args constructor
+        System.out.println(cs1);//same thing with toString
+        cs1.setId(1);
+        cs1.setName("Bob");
+        System.out.println(cs1);
     }
 }

a53d1f23a79494962d8750d1c6eb90d9d8480f76
FaithSlade <faithwslade@gmail.com>
2021-05-29 13:26:36 -0700

    Added more change in busObjects class

--- src/day48_constructors_static/BusObjects.java
+++ src/day48_constructors_static/BusObjects.java
@@ -8,5 +8,14 @@ public class BusObjects {
         bus.driver = new Driver("Abbas");
         bus.engines = new Engines(10);
         System.out.println(bus.toString());
+
+        Bus metroBus = new Bus();
+        metroBus.driver = new Driver("Aziz");
+        metroBus.engines = new Engines(15);
+
+        System.out.println(metroBus);
+
+        //print Driver name of metroBus
+        System.out.println(metroBus.driver.getName());
     }
 }

0c468e42a2dd3ff4603120bd484eb1baea3c81db
FaithSlade <faithwslade@gmail.com>
2021-05-29 09:08:27 -0700

    Added arrays and arraylist.

--- src/day48_constructors_static/CustomerObjects.java
+++ src/day48_constructors_static/CustomerObjects.java
@@ -1,5 +1,9 @@
 package day48_constructors_static;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CustomerObjects {
     public static void main(String[] args) {
         Customer cs1 = new Customer();
@@ -15,5 +19,21 @@ public class CustomerObjects {
         System.out.println(cs2);
         System.out.println(cs3);
 
+        //array of Customers
+        Customer[] todaysCustomers = {cs1, cs2, cs3, new Customer("Bashir", 449)};
+
+        //arrayList of Customers object
+        List<Customer> CustomerList = new ArrayList<>();//<> you can put anything in here like <Customer>
+        CustomerList.add(cs1);
+        CustomerList.add(cs2);
+        CustomerList.add(cs3);
+        CustomerList.add(new Customer("Bashir", 449));
+        CustomerList.add(new Customer("Derek", 9763));
+        CustomerList.add(new Customer("Nidnoi", 8907));
+        CustomerList.add(new Customer("Foam", 5476));
+
+        //Print info of first customer object in arrays and arraylist.
+        System.out.println(todaysCustomers[0].toString());//Customer{name='Bob', id=1}
+        System.out.println(todaysCustomers[0]);//Customer{name='Bob', id=1}
     }
 }

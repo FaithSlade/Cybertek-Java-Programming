a03e50e91f6f2d78d8adf4cd36c70efcab30173b
FaithSlade <faithwslade@gmail.com>
2021-06-12 11:53:07 -0700

    after lunch

--- src/day52_inheritance/Discord.java
+++ src/day52_inheritance/Discord.java
@@ -4,6 +4,23 @@ public class Discord extends MobileApp{
     public void chat(String someone){
         System.out.println("Chatting with " + someone + " on Discord");
     }
+//   Overloading method
+//    protected boolean download(double version) {
+//        System.out.println("Downloaded Discord " + version);
+//        return super.download();
+//    }
+
+    /**
+     * override download method
+     * @Override you don't need to import it, it come with java. lang.*
+     */
+
+    @Override//protected example
+    public boolean download(){
+        System.out.println("Downloaded Discord");
+        return true;
+    }
+
     @Override
     public void useTheApp(int minutes) {
         super.useTheApp(minutes);

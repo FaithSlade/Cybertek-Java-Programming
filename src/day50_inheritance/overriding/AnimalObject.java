f02460c03fc3b10b0cdddc522aafb1ca59dbfd82
FaithSlade <faithwslade@gmail.com>
2021-06-12 07:48:07 -0700

    commit all

---
+++ src/day50_inheritance/overriding/AnimalObject.java
@@ -0,0 +1,11 @@
+package day50_inheritance.overriding;
+
+public class AnimalObject {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.name = "Gray";
+        animal.type = "Pitbul";
+        animal.speak();
+
+    }
+}
